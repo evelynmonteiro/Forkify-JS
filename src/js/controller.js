16ca458e9b6be62da0049bc258f4aea5728cfed2
Evelyn Monteiro <evelyn.monteiro@3stecnologia.com.br>
2023-05-02 10:01:11 -0300

    Forkify project first commit (class 293)

---
+++ src/js/controller.js
@@ -0,0 +1,30 @@
+import * as model from './model.js'
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function(){
+  try{
+    const id = window.location.hash.slice(1)
+    
+    if(!id) return;
+    recipeView.renderSpinner()
+    
+    // 1) Loading recipe
+    await model.loadRecipe(id)
+
+    // 2) Redering recipe
+    recipeView.render(model.state.recipe)
+
+  } catch(err){
+    alert(err)
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
